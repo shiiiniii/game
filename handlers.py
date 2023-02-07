11f6797e8a26a52ed92878d9a421ce3f90433585
shiiiniii <115028625+shiiiniii@users.noreply.github.com>
2023-02-07 17:15:12 +0300

    Update handlers.py

--- handlers.py
+++ handlers.py
@@ -1,11 +1,15 @@
 from create import dp
 from aiogram import types
+from keyboards import kb_main_menu
+
+
 total = 150
 
 
 @dp.message_handler(commands=['start'])
 async def mes_start(message: types.Message):
-    await message.answer(f'Привет, {message.from_user.first_name} мы будем играть с тобой в конфеты! )')
+    await message.answer(f'Привет, {message.from_user.first_name} мы будем играть с тобой в конфеты!)', reply_markup=kb_main_menu)
+
 
 
 @dp.message_handler(commands=['help'])
@@ -13,7 +17,7 @@ async def mes_help(message: types.Message):
     await message.answer('Бот создан для игры с конфетками. Сначала с помощью команды **/set количество конфет** можно '
                          'установить максимальное количество конфет. Потом каждый игрок по очереди будет брать '
                          'определенное количество конфет. Учтите! За раз можно взять не более 28 конфет. Побеждает '
-                         'тот, у кого в итоге окажется больше всего конфет)')
+                         'тот, у кого в итоге окажется больше всего конфет)', reply_markup=kb_main_menu)
 
 @dp.message_handler(commands=['set'])
 async def mes_settings(message: types.Message):
@@ -22,6 +26,10 @@ async def mes_settings(message: types.Message):
     total = count
     await message.answer(f' Максимальное количество конфет установлено - {total}. Теперь вы можете брать конфетки. '
                          f'Введите нужное количество: ')
+
+@dp.message_handler(commands=['stop'])
+async def stop(message: types.Message):
+    await message.answer('Прощай...')
 
 
 @dp.message_handler()
@@ -34,6 +42,7 @@ async def mes_smth(message: types.Message):
         await message.answer('Введите корректное количество конфет')
     if total <= 28:
         await message.answer('Ура! Эта игра окончена!')
+
 
 
 
